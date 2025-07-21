c981c327bfdcbd3d0642498ae9a57007ebae5324
simon-forman <forrai.zoltan66@gmail.com>
2025-07-21 17:18:58 +0200

    add front page

--- src/script.js
+++ src/script.js
@@ -1,19 +1,19 @@
 function loadMarkdownFromHash() {
-  const slug = location.hash.slice(1) || 'home'; // fallback to home
+  const slug = location.hash.slice(1) || "home"; // fallback to home
   const file = `md/${slug}.md`;
 
   fetch(file)
-    .then(res => {
-      if (!res.ok) throw new Error('Not found');
+    .then((res) => {
+      if (!res.ok) throw new Error("Not found");
       return res.text();
     })
-    .then(md => {
-      document.getElementById('markdown-box').innerHTML = marked.parse(md);
+    .then((md) => {
+      document.getElementById("markdown-box").innerHTML = marked.parse(md);
     })
     .catch(() => {
-      document.getElementById('markdown-box').innerHTML = '<p>404 - Page not found</p>';
+      window.location.href = "https://forrai-zoltan.github.io/404";
     });
 }
 
-window.addEventListener('hashchange', loadMarkdownFromHash);
-window.addEventListener('DOMContentLoaded', loadMarkdownFromHash);
+window.addEventListener("hashchange", loadMarkdownFromHash);
+window.addEventListener("DOMContentLoaded", loadMarkdownFromHash);
